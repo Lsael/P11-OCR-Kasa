da267bf440be5ca5a5ffe1e16d3929180403aae4
Lsael <l.saelens@proton.me>
2022-10-24 12:55:18 +0200

    Added some style on thumbnails

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import homeBanner from '../assets/home-banner.png'
 
 const Home = () => {
   return (
-    <section>
+    <section className='home'>
       <div className="banner home__banner">
         <img src={homeBanner} alt="Montagnes embrumées" />
         <h2>Chez vous,<br/> partout et ailleurs</h2>
@@ -11,7 +11,7 @@ const Home = () => {
       {
         products.map((product) => {
           return(
-            <article className='.Thumb'>
+            <article className='Thumb'>
               <img src={product.cover} alt={product.title} />
               <h3>{product.title}</h3>
             </article>
