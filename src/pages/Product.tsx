d809dab7c7db3afc675b9fb5b7b7a53a747e6014
Lsael <l.saelens@proton.me>
2022-10-25 17:44:08 +0200

    Beginned to build product page component

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,7 +1,18 @@
+import products from '../database/db.json';
+
 
 const Lodging = () => {
+    const id = window.location.href.split('/logements/')[1];
+    let product;
+
+    for(let i=0; i < products.length; i++) {
+        if(products[i].id === id) {
+            product = products[i]
+        }
+    }
+    console.log(product);
     return(
-        <div>Le logement que je consulte</div>
+        <div>Le logement que je consulte a l'id : {product?.id} </div>
     )
 }
 
