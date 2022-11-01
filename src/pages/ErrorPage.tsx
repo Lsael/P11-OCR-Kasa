12a8e41238e6aafa120eee83bf21c9eb89112e79
Lsael <l.saelens@proton.me>
2022-11-01 16:29:21 +0100

    Upgraded error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,25 +1,35 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import Layout from "./Layout";
 
 export const ErrorPage = () => {
   const error:any = useRouteError();
-  console.error(error);
 
   if(error.status === 404) {
-      return (
-        <div id="error-page">
-          <h1>404</h1>
-          <p>Oups! La page que vous demandez n'existe pas !</p>
+    return (
+      <Layout>
+        <div className="error-page">
+          <div>
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas !</p>
+          </div>
+          <Link to="/accueil">Retourner sur la page d’accueil</Link>
         </div>
+      </Layout>
       );
   } else {
-      return (
-        <div id="error-page">
-          <h1>Oops!</h1>
-          <p>Sorry, an unexpected error has occurred.</p>
-          <p>
-            <i>{error.statusText || error.message}</i>
-          </p>
+    return (
+      <Layout>
+        <div className="error-page">
+          <div>
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+              <i>{error.statusText || error.message}</i>
+            </p>
+          </div>
+          <Link to="/accueil">Retourner sur la page d’accueil</Link>
         </div>
-      );
+      </Layout>
+    );
   }
 }
