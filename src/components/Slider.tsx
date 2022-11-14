012554b2d7646829e2608e32f770b02da272b714
Lsael <l.saelens@proton.me>
2022-11-15 00:11:24 +0100

    Added pagination on product page

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,11 @@
-import { useState} from 'react';
+import { useEffect, useState} from 'react';
 import arrow from '../assets/arrow_back_ios-24px 2.png';
 import { productImages } from '../interfaces';
 
 const Slider = (images:productImages) => {
-    const [imageIndex,setImageIndex] = useState(0);
     const covers:string[] = images.images;
-    
+    const [imageIndex,setImageIndex] = useState(0);
+    const [pagin, setPageIn] = useState('');
     const slideLeft = () => {
         if(imageIndex > 0) {
             setImageIndex(imageIndex - 1)
@@ -15,17 +15,22 @@ const Slider = (images:productImages) => {
     }
 
     const slideRight = () => {
-        if(imageIndex < (covers.length - 1)) {
+        if(imageIndex < covers.length - 1) {
             setImageIndex(imageIndex + 1)
         } else {
             setImageIndex(0)
         }
     }
+    
+    useEffect(() => {
+        setPageIn(`${imageIndex + 1}/${covers.length}`)
+    },[imageIndex,covers.length])
 
     return(
         <div className="slider">
             <img src={covers[imageIndex]} alt="logement" />
             <img src={arrow} alt="" className='arrow arrow__left' onClick={() => slideLeft()}/>
+            <p className="pagin">{pagin}</p>
             <img src={arrow} alt="" className='arrow arrow__right' onClick={() => slideRight()}/>
         </div>
     )
