8d24202895f4aa1fda09db663c1ddf0258c59637
Lsael <l.saelens@proton.me>
2022-11-22 19:27:02 +0100

    Modified slider component to remove useless useEffect

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState} from 'react';
+import { useState} from 'react';
 import arrow from '../assets/arrow_back_ios-24px 2.png';
 import { productImages } from '../interfaces';
 
 const Slider = (images:productImages) => {
     const covers:string[] = images.images;
     const [imageIndex,setImageIndex] = useState(0);
-    const [pagin, setPageIn] = useState('');
     const slideLeft = () => {
         if(imageIndex > 0) {
             setImageIndex(imageIndex - 1)
@@ -21,17 +20,13 @@ const Slider = (images:productImages) => {
             setImageIndex(0)
         }
     }
-    
-    useEffect(() => {
-        setPageIn(`${imageIndex + 1}/${covers.length}`)
-    },[imageIndex,covers.length])
 
     if(covers.length > 1) {
         return(
             <div className="slider">
                 <img src={covers[imageIndex]} alt="logement" />
                 <img src={arrow} alt="" className='arrow arrow__left' onClick={() => slideLeft()}/>
-                <p className="pagin">{pagin}</p>
+                <p className="pagin">{imageIndex + 1}/{covers.length}</p>
                 <img src={arrow} alt="" className='arrow arrow__right' onClick={() => slideRight()}/>
             </div>
         )
